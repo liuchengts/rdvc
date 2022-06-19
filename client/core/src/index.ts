9925f8441ddb001409e40fdfef317ae0647bceaf
刘成 <790809568@qq.com>
2022-06-19 22:33:41 +0800

    重构完成

--- client/core/src/index.ts
+++ client/core/src/index.ts
@@ -1,21 +1,26 @@
 // @ts-ignore
 import express from "express";
-import {createServer} from "http";
-// import {serverSocketService} from "./ts/socket/server";
+import {engine} from "express-handlebars";
 import {clientSocketService} from "./socket/client";
+import {router} from "./routers";
 
 const app = express();
 const port = 3000;
-const server_socket_port = 8001;
 app.use(express.static('node_modules'))
+app.use(express.static('public'))
+app.engine('html', engine({
+    layoutsDir: 'views',
+    defaultLayout: 'layout',
+    extname: '.html'
+}))
+app.set('view engine', 'html')
+app.use('/', router)
+
 const client_socket_prot = 8000
 const connection = "http://192.168.50.71:" + client_socket_prot
-// const connection = "http://localhost:" + client_socket_prot
 clientSocketService.init(connection)
-// serverSocketService.init(server_socket_port, createServer(app))
-// app.listen(port, () => {
-//     console.log("client core express:", port);
-// });
-console.log("client core start");
+app.listen(port, () => {
+    console.log("client core express:", port);
+});
 
 
