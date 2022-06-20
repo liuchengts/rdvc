60b4f657864bf6d35ded26963656c1cd6e46b41b
刘成 <790809568@qq.com>
2022-06-20 23:12:00 +0800

    111

--- client/core/src/index.ts
+++ client/core/src/index.ts
@@ -7,7 +7,8 @@ import {router} from "./routers";
 const app = express();
 const port = 3000;
 app.use(express.static('node_modules'))
-app.use(express.static('public'))
+// app.use('/api', express.static(__dirname + 'src/api'));
+console.log("__dirname:",__dirname)
 app.engine('html', engine({
     layoutsDir: 'views',
     defaultLayout: 'layout',
@@ -16,13 +17,14 @@ app.engine('html', engine({
 app.set('views', 'src/views')
 app.set('view engine', 'html')
 app.use('/', router)
+app.use('/static', express.static('src/api'));
 
 const client_socket_prot = 8000
 // const connection = "http://10.30.20.177:" + client_socket_prot
 const connection = "http://192.168.50.71:" + client_socket_prot
 clientSocketService.init(connection)
-// app.listen(port, () => {
-//     console.log("client core express:", port);
-// });
+app.listen(port, () => {
+    console.log("client core express:", port);
+});
 
 
