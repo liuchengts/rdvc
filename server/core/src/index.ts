a11f6048e8a78599af9bb9adefc16265bfa90901
cheng.liu <790809568@qq.com>
2023-07-19 16:11:11 +0800

    重写中

--- server/core/src/index.ts
+++ server/core/src/index.ts
@@ -1,26 +1,4 @@
-// @ts-ignore
-import express from "express";
-import {engine} from "express-handlebars";
-import {router} from "./routers";
-import {createServer} from "http";
-import {serverSocketService} from "./ts/socket/server";
 
-const app = express();
-const PORT = 3000;
-const SOCKET_PORT = 8000;
-app.use(express.static('public'))
-app.use(express.static('node_modules'))
-app.engine('html', engine({
-    layoutsDir: 'views',
-    defaultLayout: 'layout',
-    extname: '.html'
-}))
-app.set('view engine', 'html')
-app.use('/', router)
 
-serverSocketService.init(SOCKET_PORT, createServer(app))
-app.listen(PORT, () => {
-    console.log("Express:", PORT);
-});
 
 
